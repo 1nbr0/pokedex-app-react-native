7d426afe91575a40395c01a070f46eab6cbcbceb
Robin667 <robin.turpin8@gmail.com>
2022-03-11 00:04:25 +0100

    finalisation header + btn trois petit point et logout

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import { Layout } from "./src/components/Layout";
-import { StyleSheet, Text } from "react-native";
+import { Text } from "react-native";
 import { Header } from "./src/components/Header";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider } from "@ui-kitten/components";
 
 export default function App() {
   return (
-    <Layout>
-      <Header />
-      <Text>Pokedex App, ici la vue App.js</Text>
-    </Layout>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Layout>
+        <Header />
+        <Text>Pokedex App, ici la vue App.js</Text>
+      </Layout>
+    </ApplicationProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
